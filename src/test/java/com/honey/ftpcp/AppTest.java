eb10fdb253afb24ca33a56ce50b334296cf32b69
honwhy.wang <honwhy19@163.com>
2018-01-10 23:44:30 +0800

    initial commit

---
+++ src/test/java/com/honey/ftpcp/AppTest.java
@@ -0,0 +1,16 @@
+package com.honey.ftpcp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
